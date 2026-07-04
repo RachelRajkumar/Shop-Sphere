2e8a21a4132e3173ab31aed3e5ddad2eb5cb6210
Rachel <rachelprincy55@gmail.com>
2026-07-04 21:19:48 +0530

    Added STATIC_ROOT for Render deployment

--- shopsphere/settings.py
+++ shopsphere/settings.py
@@ -100,7 +100,7 @@ USE_TZ = True
 
 # ---------------- STATIC FILES ----------------
 STATIC_URL = '/static/'
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # ---------------- MEDIA FILES ----------------
 MEDIA_URL = '/media/'
